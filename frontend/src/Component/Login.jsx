20033a22be7d77c02e041089266dfcf43bc24787
RAGAV-24 <ragavrunner33@gmail.com>
2025-03-13 12:02:40 +0530

    first commit 2

--- frontend/src/Component/Login.jsx
+++ frontend/src/Component/Login.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { useState } from "react";
 import logo from "../assets/react.svg";
+import axios from "axios";
 import { useNavigate } from 'react-router-dom';
 const Login = () => {
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("");
+  const [error, setError] = useState("");
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:8000/api/auth/login", {
+        email,
+        password,
+      });
+      localStorage.setItem("token", response.data.token);
+      console.log("Login successful!", response.data);
+      navigate("/dashboard");
+    } catch (err) {
+      console.error("Login failed:", err.response?.data?.message);
+      setError(err.response?.data?.message || "Login failed");
+    }
+  };
   const handleAccount = () => {
     navigate("/signup");
   }
@@ -15,14 +36,17 @@ const Login = () => {
       </div>
       <div className=" bg-white rounded-lg shadow-2xl h-100 w-100 text-black p-3">
         <h1 className="text-3xl font-bold">Login</h1>
-        <form>
-          <p >UserName:</p>
-          <input type="text" placeholder="Username" className="block border border-grey-light w-full p-3 rounded mb-4" />
+        {error && <p className="text-red-500">{error}</p>}
+        <form onSubmit={handleLogin}>
+          <p >Email:</p>
+          <input type="text" placeholder="email" className="block border border-grey-light w-full p-3 rounded mb-4" onChange={(e) => setEmail(e.target.value)} value={email} />
           <p  >Password:</p>
-          <input placeholder="password" type="password" className="block border border-grey-light w-full p-3 rounded mb-4" />
+          <input placeholder="password" type="password" className="block border border-grey-light w-full p-3 rounded mb-4" value={password}
+              onChange={(e) => setPassword(e.target.value) }/>
           <p  >Role:</p>
-          <select className="block border border-grey-light w-full p-3 rounded mb-4">
-  <option value="" disabled selected>Select your role</option>
+          <select className="block border border-grey-light w-full p-3 rounded mb-4" value={role}
+              onChange={(e) => setRole(e.target.value)}>
+  <option value="" disabled selected >Select your role</option>
   <option value="student">Admin</option>
   <option value="teacher">Manager</option>
   <option value="event_coordinator">Employee</option>
