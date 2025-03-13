c939def5104a6cbe4d25c6b1a10fec7a4b2de583
RAGAV-24 <ragavrunner33@gmail.com>
2025-03-13 13:15:21 +0530

    msg

--- frontend/src/Component/Signup.jsx
+++ frontend/src/Component/Signup.jsx
@@ -1,32 +1,64 @@
-import React from 'react'
+import React,{useState} from 'react'
 import logo from "../assets/react.svg";
-
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 const Signup = () => {
   const navigate = useNavigate();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const handleSignup = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:8000/api/auth/signup", {
+        name,
+        email,
+        password,
+      });
+
+      console.log("Signup successful!", response.data);
+      setSuccess("Account created successfully! Redirecting...");
+
+      setTimeout(() => navigate("/login"), 2000);
+    } catch (err) {
+      console.error("Signup failed:", err.response?.data?.message);
+      setError(err.response?.data?.message || "Signup failed");
+    }
+  };
   const handleAccount = () => {
-    navigate("/login");
+    navigate("/");
   }
   return (
     <div>
       <div className="flex justify-center items-center h-screen bg-gray-500">
+        <div>{success}</div>
        <div className=" bg-black rounded-lg flex justify-center items-center pl-10 pt-5 w-100 h-100">
        <img src={logo} className="h-100"></img>
        </div>
        <div className="bg-white rounded-lg shadow-2xl h-100 w-100 text-black p-3">
         <h1 className="text-3xl font-bold">Signup</h1>
+        {error && <p className="text-red-500">{error}</p>}
+        <form onSubmit={handleSignup}>
         <p className="">Name</p>
-        <input type="text" placeholder="Name" className="block border border-grey-light w-full p-3 rounded mb-4" />
+        <input type="text" placeholder="Name" className="block border border-grey-light w-full p-3 rounded mb-4"  value={name}
+            onChange={(e) => setName(e.target.value)}/>
         <p className="">Password</p>
-        <input type="password" placeholder="Password" className="block border border-grey-light w-full p-3 rounded mb-4" />
+        <input type="password" placeholder="Password" className="block border border-grey-light w-full p-3 rounded mb-4"   value={password}
+            onChange={(e) => setPassword(e.target.value)}/>
         <p className="">Email</p>
-        <input type="email" placeholder="Email" className="block border border-grey-light w-full p-3 rounded mb-4" />
+        <input type="email" placeholder="Email" className="block border border-grey-light w-full p-3 rounded mb-4"   value={email}
+            onChange={(e) => setEmail(e.target.value)} />
         <button className="w-full text-center py-3 rounded bg-black text-white hover:bg-gray-400 focus:outline-none my-1">Signup
 
         </button>
+        </form>
         <div>
         <p>
-          Already Have an account <span onClick={handleAccount} className="underline text-blue-900"> Login </span>
+          Already Have an account{" "}<span onClick={handleAccount} className="underline text-blue-900"> Login </span>
         </p>
         </div>
 
