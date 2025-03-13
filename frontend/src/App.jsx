5cf2029c73da4e63dbc9b99cf61195b91a4d820b
RAGAV-24 <ragavrunner33@gmail.com>
2025-03-13 14:37:58 +0530

    creating DashBoard compoenent

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Component/Login';
 import Signup from './Component/Signup'
+import DashBoard from './Component/DashBoard'
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<DashBoard />} />
       </Routes>
     </Router>
   );
